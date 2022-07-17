a52fee1c5633be172b62bd2b1aaa79c296a5fe15
XGit3000 <sebastian.carrier@gmail.com>
2022-07-17 09:35:42 -0400

    death screen looking snazzy now to make it sound funky

--- assets/scripts/UI/Gameplay UI.gd
+++ assets/scripts/UI/Gameplay UI.gd
@@ -5,6 +5,12 @@ extends Control
 # var a = 2
 # var b = "text"
 var player_max_health = 100.0
+export(float) var timey_scale = 1 setget set_timey_scale
+
+func set_timey_scale(n_scale):
+	timey_scale = n_scale
+	Engine.time_scale = timey_scale
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,3 +30,32 @@ func recieve_player_health(health):
 
 func recieve_player_max_health(max_health):
 	player_max_health = max_health
+
+func recieve_player_death():
+#	Engine.time_scale = 0.2
+	$DEATH.visible = true
+	$AnimationPlayer.play("DEATH")
+	$"DEATH/CenterContainer/VBoxContainer/RETRY BUTTON".grab_focus()
+
+
+func _on_RETRY_BUTTON_pressed():
+	get_tree().reload_current_scene()
+	pass # Replace with function body.
+
+
+func _on_EXIT_BUTTON_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "DEATH":
+		Engine.time_scale = 1
+		get_tree().paused = true
+	pass # Replace with function body.
+
+func _input(event):
+	if (event.is_action_pressed("pause") or event.is_action_pressed("ui_cancel")) and $DEATH.visible:
+		get_tree().quit()
+#	if event.is_action_pressed()
